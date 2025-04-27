2ebe9cee2b84a67cef8724039fead848f6c78e05
Cleeein <pagaroganjhonclein@gmail.com>
2025-04-27 15:13:32 +0800

    redesigned topFive page and added my top five (still needs some designing)

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 $("document").ready(() => {
 
-    $.getJSON("https://pokeapi.co/api/v2/pokemon/charizard", (data) => {
+    const topFive = ["greninja", "blastoise", "mewtwo", "charizard",  "blaziken"];
+    let count = 1;
+    topFive.forEach(pokemon => {
+        $.getJSON(`https://pokeapi.co/api/v2/pokemon/${pokemon}`, (data) => {
 
-        $("#poke-charizard").text(data.name);
-        $("#poke-type").text(data.types[0].type.name);       
-        $("#poke-charizardimg").attr("src", data.sprites.front_default);
-        data.stats.forEach(stat => {
-            const statName = stat.stat.name.replace("-", "_");
-            const statValue = stat.base_stat;
+            $(`.pokedex-card${count}`).addClass(`${data.types[0].type.name}-type`); 
+            $(`#poke-name${count}`).text(data.name);
+            $(`#poke-type${count}`).text(data.types[0].type.name);           
+            
+            $(`#poke-sprite${count}`).attr("src", data.sprites.front_shiny);
+            data.stats.forEach(stat => {
+                const statName = stat.stat.name.replace("-", "_");
+                const statValue = stat.base_stat;
+    
+                $(`#stat-${statName}${count}`).text(statValue);
+            });
 
-            $(`#stat-${statName}`).text(statValue);
+            count++;
+
+            // const $newDiv = $("<div></div>");
+            // $newDiv.addClass("poke-card");
+            // $newDiv.text = (data.name);
+            // $("row").append($newDiv);
         });
+    
     });
-
-    $.getJSON("https://pokeapi.co/api/v2/pokemon/greninja", (data) => {
-        $("#poke-greninja").text(data.name);
-        $("#poke-type-g").text(data.types[0].type.name);
-        $("#poke-greninja-img").attr("src", data.sprites.front_default);
-        data.stats.forEach(stat => {
-            const statName = stat.stat.name.replace("-", "_");
-            const statValue = stat.base_stat;
-
-            $(`#stat-${statName}-g`).text(statValue);
-        });
-    });
-
     
 });
